a8f02aa2cf8bee8606b1c6eb32cbbe1501fb580b
Yan Malinovsky <yan.malinovsky@gmail.com>
2019-05-13 13:29:54 +0300

    New base URL

--- CordialSDK/CordialApiConfiguration.swift
+++ CordialSDK/CordialApiConfiguration.swift
@@ -28,7 +28,7 @@ public class CordialApiConfiguration {
     public func initialize(accountKey: String, channelKey: String) {
         self.accountKey = accountKey
         self.channelKey = channelKey
-        self.baseURL = "https://events-stream.svc.mobile-sdk.cordialdev.com/"
+        self.baseURL = "https://events-stream-svc.skrivobocheck.cordialdev.com/"
         
 //        CoreDataManager.shared.deleteAllCoreData()
         
