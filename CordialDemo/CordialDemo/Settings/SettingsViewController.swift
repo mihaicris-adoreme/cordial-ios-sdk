e05fc0a8560ecf1a647636cb8da9e271a2797b9c
Yan Malinovsky <yan.malinovsky@gmail.com>
2019-05-30 15:07:11 +0300

    Qty cached event queue

--- CordialDemo/CordialDemo/Settings/SettingsViewController.swift
+++ CordialDemo/CordialDemo/Settings/SettingsViewController.swift
@@ -14,6 +14,7 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var baseURLTextField: UITextField!
     @IBOutlet weak var accountKeyTextField: UITextField!
     @IBOutlet weak var channelKeyTextField: UITextField!
+    @IBOutlet weak var qtyCachedEventQueueTextField: UITextField!
     
     let cordialAPI = CordialAPI()
     
@@ -23,10 +24,12 @@ class SettingsViewController: UIViewController {
         baseURLTextField.setBottomBorder(color: UIColor.gray)
         accountKeyTextField.setBottomBorder(color: UIColor.gray)
         channelKeyTextField.setBottomBorder(color: UIColor.gray)
+        qtyCachedEventQueueTextField.setBottomBorder(color: UIColor.gray)
         
         baseURLTextField.text = cordialAPI.getBaseURL()
         accountKeyTextField.text = cordialAPI.getAccountKey()
         channelKeyTextField.text = cordialAPI.getChannelKey()
+        qtyCachedEventQueueTextField.text = String(CordialApiConfiguration.shared.qtyCachedEventQueue)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -34,10 +37,11 @@ class SettingsViewController: UIViewController {
     }
 
     func saveTestInitData() {
-        if let baseURL = baseURLTextField.text, let accountKey = accountKeyTextField.text, let channelKey = channelKeyTextField.text {
+        if let baseURL = self.baseURLTextField.text, let accountKey = self.accountKeyTextField.text, let channelKey = self.channelKeyTextField.text, let qtyCachedEventQueueString = self.qtyCachedEventQueueTextField.text, let qtyCachedEventQueue = Int(qtyCachedEventQueueString) {
             cordialAPI.setBaseURL(baseURL: baseURL)
             cordialAPI.setAccountKey(accountKey: accountKey)
             cordialAPI.setChannelKey(channelKey: channelKey)
+            CordialApiConfiguration.shared.qtyCachedEventQueue = qtyCachedEventQueue
         }
     }
 }
