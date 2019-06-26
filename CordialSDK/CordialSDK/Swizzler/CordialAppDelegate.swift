e7abcfbe3220659dc6dd2239e3a9cfbd2812f0d6
Yan Malinovsky <yan.malinovsky@gmail.com>
2019-06-26 16:49:10 +0300

    Change public to open CordialAppDelegate

--- CordialSDK/CordialSDK/Swizzler/CordialAppDelegate.swift
+++ CordialSDK/CordialSDK/Swizzler/CordialAppDelegate.swift
@@ -12,23 +12,23 @@ open class CordialAppDelegate: UIResponder, UIApplicationDelegate {
     
     // MARK: - Handle remote notification registration.
     
-    public func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    open func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         
     }
     
-    public func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+    open func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
         
     }
     
     // MARK: Handle universal links
     
-    public func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+    open func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
         return false
     }
     
     // MARK: Handle URL schemes
     
-    public func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    open func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
         return false
     }
     
