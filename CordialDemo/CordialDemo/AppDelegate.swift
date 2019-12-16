ab1bdcf81e54911a7df423044af7ddc77c97ba62
Yan Malinovsky <yan.malinovsky@gmail.com>
2019-12-16 15:58:40 +0200

    Adde Demo App Deployment target 10.0

--- CordialDemo/CordialDemo/AppDelegate.swift
+++ CordialDemo/CordialDemo/AppDelegate.swift
@@ -33,6 +33,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 
         self.setupCordialSDKLogicErrorHandler()
         
+        guard #available(iOS 13.0, *) else {
+            if CordialAPI().getContactPrimaryKey() != nil {
+                let catalogNavigationController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "CatalogNavigationController") as! UINavigationController
+                self.window?.rootViewController = catalogNavigationController
+            }
+            
+            return true
+        }
+        
         return true
     }
 
@@ -62,12 +71,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     // MARK: UISceneSession Lifecycle
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default", sessionRole: connectingSceneSession.role)
     }
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
--- CordialDemo/CordialDemo/DeepLinks/CordialDeepLinksHandler.swift
+++ CordialDemo/CordialDemo/DeepLinks/CordialDeepLinksHandler.swift
@@ -19,9 +19,9 @@ class CordialDeepLinksHandler: CordialDeepLinksDelegate {
         }
         
         if let products = URLComponents(url: url, resolvingAgainstBaseURL: true), let product = ProductHandler.shared.products.filter({ $0.path == products.path }).first {
-            self.showDeepLink(product: product, scene: nil)
+            self.showAppDelegateDeepLink(product: product)
         } else if let fallbackURL = fallbackURL, let products = URLComponents(url: fallbackURL, resolvingAgainstBaseURL: true), let product = ProductHandler.shared.products.filter({ $0.path == products.path }).first {
-            self.showDeepLink(product: product, scene: nil)
+            self.showAppDelegateDeepLink(product: product)
         } else if let webpageUrl = URL(string: "https://\(host)/") {
             UIApplication.shared.open(webpageUrl)
         }
@@ -34,15 +34,15 @@ class CordialDeepLinksHandler: CordialDeepLinksDelegate {
         }
         
         if let products = URLComponents(url: url, resolvingAgainstBaseURL: true), let product = ProductHandler.shared.products.filter({ $0.path == products.path }).first {
-            self.showDeepLink(product: product, scene: scene)
+            self.showSceneDelegateDeepLink(product: product, scene: scene)
         } else if let fallbackURL = fallbackURL, let products = URLComponents(url: fallbackURL, resolvingAgainstBaseURL: true), let product = ProductHandler.shared.products.filter({ $0.path == products.path }).first {
-            self.showDeepLink(product: product, scene: scene)
+            self.showSceneDelegateDeepLink(product: product, scene: scene)
         } else if let webpageUrl = URL(string: "https://\(host)/") {
             UIApplication.shared.open(webpageUrl)
         }
     }
     
-    private func showDeepLink(product: Product, scene: UIScene?) {
+    private func showAppDelegateDeepLink(product: Product) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         
         let productViewController = storyboard.instantiateViewController(withIdentifier: "ProductViewController") as! ProductViewController
@@ -51,10 +51,23 @@ class CordialDeepLinksHandler: CordialDeepLinksDelegate {
         let catalogNavigationController = storyboard.instantiateViewController(withIdentifier: "CatalogNavigationController") as! UINavigationController
         catalogNavigationController.pushViewController(productViewController, animated: false)
         
-        if let scene = scene, let sceneDelegate = scene.delegate as? SceneDelegate {
-            sceneDelegate.window?.rootViewController = catalogNavigationController
-        } else if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
             appDelegate.window?.rootViewController = catalogNavigationController
+        }
+    }
+    
+    @available(iOS 13.0, *)
+    private func showSceneDelegateDeepLink(product: Product, scene: UIScene) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        let productViewController = storyboard.instantiateViewController(withIdentifier: "ProductViewController") as! ProductViewController
+        productViewController.product = product
+        
+        let catalogNavigationController = storyboard.instantiateViewController(withIdentifier: "CatalogNavigationController") as! UINavigationController
+        catalogNavigationController.pushViewController(productViewController, animated: false)
+        
+        if let sceneDelegate = scene.delegate as? SceneDelegate {
+            sceneDelegate.window?.rootViewController = catalogNavigationController
         }
     }
 }
