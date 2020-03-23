d8c337127366abc6d2d69511aeaa01428d633d0c
Yan Malinovsky <yan.malinovsky@gmail.com>
2020-03-23 07:33:07 +0200

    Added view to GeoViewController

--- CordialDemo/CordialDemo/Profile/Geo/GeoViewController.swift
+++ CordialDemo/CordialDemo/Profile/Geo/GeoViewController.swift
@@ -12,12 +12,35 @@ class GeoViewController: UIViewController, UINavigationControllerDelegate {
     
     var attributesViewController: AttributesViewController?
     
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var countryTextField: UITextField!
+    @IBOutlet weak var postalCodeTextField: UITextField!
+    @IBOutlet weak var stateTextField: UITextField!
+    @IBOutlet weak var streetAdressTextField: UITextField!
+    @IBOutlet weak var streetAdress2TextField: UITextField!
+    @IBOutlet weak var timeZoneTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.title = "Geo"
         
         self.navigationController?.delegate = self
+        
+        self.cityTextField.setBottomBorder(color: UIColor.lightGray)
+        self.countryTextField.setBottomBorder(color: UIColor.lightGray)
+        self.postalCodeTextField.setBottomBorder(color: UIColor.lightGray)
+        self.stateTextField.setBottomBorder(color: UIColor.lightGray)
+        self.streetAdressTextField.setBottomBorder(color: UIColor.lightGray)
+        self.streetAdress2TextField.setBottomBorder(color: UIColor.lightGray)
+        self.timeZoneTextField.setBottomBorder(color: UIColor.lightGray)
+        
+    }
+    
+    @IBAction func addGeoAttributeAction(_ sender: UIBarButtonItem) {
+        if let viewControllers = self.navigationController?.viewControllers, viewControllers.count > 3 {
+             self.navigationController?.popToViewController(viewControllers[viewControllers.count - 3], animated: true)
+         }
     }
     
     // MARK: UINavigationControllerDelegate
