37b54f59339d00c8cc6ed9d86f44cc26bbb6f10b
Yan Malinovsky <yan.malinovsky@gmail.com>
2020-03-25 06:00:48 +0200

    Changed view of tableView cell at ProfileViewController

--- CordialDemo/CordialDemo/Profile/Profile/ProfileViewController.swift
+++ CordialDemo/CordialDemo/Profile/Profile/ProfileViewController.swift
@@ -72,24 +72,85 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
 
         let attribute = self.attributes[indexPath.row]
         
-        cell.typeLabel.text = attribute.type.rawValue.capitalized
-        cell.keyLabel.text = attribute.key
+        let titleAttributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 15)]
+        let valueAttributes = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 15)]
         
-        var value = Attribute.performArrayToStringSeparatedByComma(attribute.value)
+        let typeString = attribute.type.rawValue.capitalized
+        cell.typeLabel.attributedText = NSAttributedString(string: typeString, attributes: valueAttributes)
+        
+        let keyString = attribute.key
+        cell.keyLabel.attributedText = NSAttributedString(string: keyString, attributes: valueAttributes)
+        
+        let valueString = Attribute.performArrayToStringSeparatedByComma(attribute.value)
+        var value = NSAttributedString(string: valueString, attributes: valueAttributes)
         
         switch attribute.type {
         case AttributeType.date:
-            let date = CordialDateFormatter().getDateFromTimestamp(timestamp: value)!
-            value = AppDateFormatter().getTimestampFromDate(date: date)
+            let date = CordialDateFormatter().getDateFromTimestamp(timestamp: valueString)!
+            let dateString = AppDateFormatter().getTimestampFromDate(date: date)
+            value = NSAttributedString(string: dateString, attributes: valueAttributes)
         case AttributeType.geo:
             if let appDelegate = UIApplication.shared.delegate as? AppDelegate, let geoAttribute = AppDataManager.shared.geoAttributes.getGeoAttributeFromCoreDataByKey(appDelegate: appDelegate, key: attribute.key) {
-                value = "City:\t\t\t\t\(geoAttribute.city)\nCountry:\t\t\t\(geoAttribute.country)\nPostal Code:\t\t\(geoAttribute.postalCode)\nState:\t\t\t\t\(geoAttribute.state)\nStreet Adress:\t\t\(geoAttribute.streetAdress)\nStreet Adress 2:\t\(geoAttribute.streetAdress2)\nTime Zone:\t\t\t\(geoAttribute.timeZone)"
+                
+                let cityTitle = NSAttributedString(string: "City: ", attributes: titleAttributes)
+                let countryTitle = NSAttributedString(string: "Country: ", attributes: titleAttributes)
+                let postalCodeTitle = NSAttributedString(string: "Postal Code: ", attributes: titleAttributes)
+                let stateTitle = NSAttributedString(string: "State: ", attributes: titleAttributes)
+                let streetAdressTitle = NSAttributedString(string: "Street Adress: ", attributes: titleAttributes)
+                let streetAdress2Title = NSAttributedString(string: "Street Adress 2: ", attributes: titleAttributes)
+                let timeZoneTitle = NSAttributedString(string: "Time Zone: ", attributes: titleAttributes)
+                
+                let cityValue = NSAttributedString(string: "\(geoAttribute.city)", attributes: valueAttributes)
+                let countryValue = NSAttributedString(string: "\(geoAttribute.country)", attributes: valueAttributes)
+                let postalCodeValue = NSAttributedString(string: "\(geoAttribute.postalCode)", attributes: valueAttributes)
+                let stateValue = NSAttributedString(string: "\(geoAttribute.state)", attributes: valueAttributes)
+                let streetAdressValue = NSAttributedString(string: "\(geoAttribute.streetAdress)", attributes: valueAttributes)
+                let streetAdress2Value = NSAttributedString(string: "\(geoAttribute.streetAdress2)", attributes: valueAttributes)
+                let timeZoneValue = NSAttributedString(string: "\(geoAttribute.timeZone)", attributes: valueAttributes)
+                
+                let newLine = NSAttributedString(string: "\n", attributes: titleAttributes)
+                
+                let geoValue = NSMutableAttributedString()
+                geoValue.append(cityTitle)
+                geoValue.append(cityValue)
+                
+                geoValue.append(newLine)
+                
+                geoValue.append(countryTitle)
+                geoValue.append(countryValue)
+                
+                geoValue.append(newLine)
+                
+                geoValue.append(postalCodeTitle)
+                geoValue.append(postalCodeValue)
+                
+                geoValue.append(newLine)
+                
+                geoValue.append(stateTitle)
+                geoValue.append(stateValue)
+                
+                geoValue.append(newLine)
+                
+                geoValue.append(streetAdressTitle)
+                geoValue.append(streetAdressValue)
+                
+                geoValue.append(newLine)
+                
+                geoValue.append(streetAdress2Title)
+                geoValue.append(streetAdress2Value)
+                
+                geoValue.append(newLine)
+                
+                geoValue.append(timeZoneTitle)
+                geoValue.append(timeZoneValue)
+                
+                value = geoValue
             }
         default:
             break
         }
         
-        cell.valueLabel.text = value
+        cell.valueLabel.attributedText = value
         
         return cell
     }
