b1ad069ddf0696ba07b56d96a4906faf73986871
Yan Malinovsky <yan.malinovsky@gmail.com>
2020-04-13 17:51:34 +0300

    Added test for systemEventsProperties

---
+++ CordialSDK/CordialSDKTests/CordialSDK/RequestSender/MockRequestSenderSystemEventsProperties.swift
@@ -0,0 +1,35 @@
+//
+//  MockRequestSenderSystemEventsProperties.swift
+//  CordialSDKTests
+//
+//  Created by Yan Malinovsky on 13.04.2020.
+//  Copyright © 2020 cordial.io. All rights reserved.
+//
+
+import XCTest
+import CordialSDK
+
+class MockRequestSenderSystemEventsProperties: RequestSender {
+    
+    var isVerified = false
+    
+    var systemEventsProperties = Dictionary<String, String>()
+    
+    override func sendRequest(task: URLSessionDownloadTask) {
+        let httpBody = task.originalRequest!.httpBody!
+        
+        if let jsonArray = try? JSONSerialization.jsonObject(with: httpBody, options: []) as? [AnyObject] {
+            let json = jsonArray.first! as! [String: AnyObject]
+            
+            let properties = json["properties"] as! Dictionary<String, String>
+            let propertiesKeys = properties.keys
+            
+            propertiesKeys.forEach { key in
+                XCTAssertEqual(properties[key], self.systemEventsProperties[key], "System events property don't match")
+            }
+            
+            self.isVerified = true
+        }
+    }
+    
+}
