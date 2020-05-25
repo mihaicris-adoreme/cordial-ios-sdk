56944eceb3a3f8c58af069a9117d57b999de1109
Yan Malinovsky <yan.malinovsky@gmail.com>
2020-05-25 19:09:42 +0300

    Removed CordialSDKTestsHelper class

--- CordialSDK/CordialSDKTests/CordialSDK/RequestSender/MockRequestSenderSetContact.swift
+++ CordialSDK/CordialSDKTests/CordialSDK/RequestSender/MockRequestSenderSetContact.swift
@@ -13,9 +13,20 @@ class MockRequestSenderSetContact: RequestSender {
     
     var isVerified = false
     
+    let sdkTests = CordialSDKTests()
+    
     override func sendRequest(task: URLSessionDownloadTask) {
         if let request = task.originalRequest, let httpBody = request.httpBody {
-            CordialSDKTestsHelper().setContactValidation(httpBody: httpBody)
+            
+            if let jsonArray = try? JSONSerialization.jsonObject(with: httpBody, options: []) as? [AnyObject] {
+                let json = jsonArray.first! as! [String: AnyObject]
+                
+                XCTAssertEqual(json["primaryKey"] as! String, self.sdkTests.testPrimaryKey, "Primary keys don't match")
+                XCTAssertEqual(json["deviceId"] as! String, self.sdkTests.testCase.getDeviceIdentifier(), "Device ids don't match")
+                XCTAssertEqual(json["status"] as! String, self.sdkTests.testCase.getPushNotificationDisallowStatus(), "Statuses keys don't match")
+            } else {
+                XCTAssert(false, "httpBody don't array json")
+            }
             
             self.isVerified = true
         }
