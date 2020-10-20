c136ea55ae2f80a92d30b52dfa56d349af31e3be
Yan Malinovsky <yan.malinovsky@gmail.com>
2020-10-20 07:25:47 +0300

    Added data to inbox messages filter screen UI

--- CordialDemo/CordialDemo/ViewControllers/InboxMessages/InboxMessagesFilter/InboxMessagesFilterViewController.swift
+++ CordialDemo/CordialDemo/ViewControllers/InboxMessages/InboxMessagesFilter/InboxMessagesFilterViewController.swift
@@ -11,6 +11,11 @@ import CordialSDK
 
 class InboxMessagesFilterViewController: UIViewController {
     
+    @IBOutlet weak var fromDateTextField: UITextField!
+    @IBOutlet weak var fromDatePicker: UIDatePicker!
+    @IBOutlet weak var toDateTextField: UITextField!
+    @IBOutlet weak var toDatePicker: UIDatePicker!
+    
     @IBOutlet weak var isReadSegmentedControl: UISegmentedControl!
     
     var inboxPageFilter: InboxPageFilter?
@@ -22,7 +27,10 @@ class InboxMessagesFilterViewController: UIViewController {
 
         self.title = "Filter"
         
-        self.prepareIsReadSegmentedControl()
+        self.fromDateTextField.setBottomBorder(color: UIColor.black)
+        self.toDateTextField.setBottomBorder(color: UIColor.black)
+        
+        self.prepareInboxPageFilterData()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -33,9 +41,67 @@ class InboxMessagesFilterViewController: UIViewController {
         }
     }
     
-    func prepareIsReadSegmentedControl() {
+    @IBAction func fromDatePickerAction(_ sender: UIDatePicker) {
+        self.isNeededInboxMessagesUpdate = true
+        
+        self.fromDateTextField.text = AppDateFormatter().getTimestampFromDate(date: sender.date)
+        
+        if let inboxPageFilter = self.inboxPageFilter {
+            self.inboxPageFilter = InboxPageFilter(isRead: inboxPageFilter.isRead, fromDate: sender.date, toDate: inboxPageFilter.toDate)
+        } else {
+            self.inboxPageFilter = InboxPageFilter(isRead: .NONE, fromDate: sender.date, toDate: nil)
+        }
+    }
+    
+    @IBAction func toDatePickerAction(_ sender: UIDatePicker) {
+        self.isNeededInboxMessagesUpdate = true
+        
+        self.toDateTextField.text = AppDateFormatter().getTimestampFromDate(date: sender.date)
+        
+        if let inboxPageFilter = self.inboxPageFilter {
+            self.inboxPageFilter = InboxPageFilter(isRead: inboxPageFilter.isRead, fromDate: inboxPageFilter.fromDate, toDate: sender.date)
+        } else {
+            self.inboxPageFilter = InboxPageFilter(isRead: .NONE, fromDate: nil, toDate: sender.date)
+        }
+    }
+    
+    @IBAction func fromDateClearAction(_ sender: UIButton) {
+        self.isNeededInboxMessagesUpdate = true
+        
+        self.fromDateTextField.text = nil
+        
+        if let inboxPageFilter = self.inboxPageFilter {
+            self.inboxPageFilter = InboxPageFilter(isRead: inboxPageFilter.isRead, fromDate: nil, toDate: inboxPageFilter.toDate)
+        } else {
+            self.inboxPageFilter = InboxPageFilter(isRead: .NONE, fromDate: nil, toDate: nil)
+        }
+    }
+    
+    @IBAction func toDateClearAction(_ sender: UIButton) {
+        self.isNeededInboxMessagesUpdate = true
+        
+        self.toDateTextField.text = nil
+        
+        if let inboxPageFilter = self.inboxPageFilter {
+            self.inboxPageFilter = InboxPageFilter(isRead: inboxPageFilter.isRead, fromDate: inboxPageFilter.fromDate, toDate: nil)
+        } else {
+            self.inboxPageFilter = InboxPageFilter(isRead: .NONE, fromDate: nil, toDate: nil)
+        }
+    }
+    
+    func prepareInboxPageFilterData() {
         if let inboxPageFilter = self.inboxPageFilter {
             self.isReadSegmentedControl.selectedSegmentIndex = inboxPageFilter.isRead.rawValue
+            
+            if let fromDate = inboxPageFilter.fromDate {
+                self.fromDatePicker.date = fromDate
+                self.fromDateTextField.text = AppDateFormatter().getTimestampFromDate(date: fromDate)
+            }
+            
+            if let toDate = inboxPageFilter.toDate {
+                self.toDatePicker.date = toDate
+                self.toDateTextField.text = AppDateFormatter().getTimestampFromDate(date: toDate)
+            }
         }
     }
 
