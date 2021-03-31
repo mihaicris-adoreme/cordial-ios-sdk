4fcc61d83e862d7c73e2ab9f3d40053b00225c53
Yan Malinovsky <yan.malinovsky@gmail.com>
2021-03-31 16:27:24 +0300

    Removed JS code for selecting IAM text

--- CordialSDK/CordialSDK/InAppMessage/InAppMessage.js
+++ CordialSDK/CordialSDK/InAppMessage/InAppMessage.js
@@ -10,11 +10,3 @@ function crdlAction(deepLink = null, eventName = null) {
    }
 }
 
-// Init head variable
-var head = document.head || document.getElementsByTagName('head')[0];
-
-// Disable users selecting text in WKWebView
-var style = document.createElement('style');
-style.type = 'text/css';
-style.appendChild(document.createTextNode('*{-webkit-touch-callout:none;-webkit-user-select:none}'));
-head.appendChild(style);
