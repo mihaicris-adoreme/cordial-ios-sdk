56213ed4f729163e8d2f92677f4906de2974ff65
Yan Malinovsky <yan.malinovsky@gmail.com>
2021-04-21 21:36:42 +0300

    Added JSONStructure whith extensions

---
+++ CordialSDK/CordialSDK/JSONStructure/JSONStructureExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  JSONStructureExtensions.swift
+//  CordialSDK
+//
+//  Created by Yan Malinovsky on 21.04.2021.
+//  Copyright © 2021 cordial.io. All rights reserved.
+//
+
+import Foundation
