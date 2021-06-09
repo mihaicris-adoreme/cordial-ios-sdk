231b9889e7d00bf607c33808116a1bf98ca6d8d5
Yan Malinovsky <yan.malinovsky@gmail.com>
2021-06-09 19:15:46 +0300

    SwiftUI demo app init commit

---
+++ CordialDemo_SwiftUI/CordialDemo_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CordialDemo_SwiftUI
+//
+//  Created by Yan Malinovsky on 09.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("SwiftUI")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CordialDemo_SwiftUI/CordialDemo_SwiftUI/CordialDemo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CordialDemo_SwiftUIApp.swift
+//  CordialDemo_SwiftUI
+//
+//  Created by Yan Malinovsky on 09.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct CordialDemo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
