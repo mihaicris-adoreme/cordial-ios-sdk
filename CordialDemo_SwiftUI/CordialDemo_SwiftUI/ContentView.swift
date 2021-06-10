43f5eedbea8f2df379200f66f379371ac0214ad5
Yan Malinovsky <yan.malinovsky@gmail.com>
2021-06-10 16:58:34 +0300

    Added deep links support

--- CordialDemo_SwiftUI/CordialDemo_SwiftUI/ContentView.swift
+++ CordialDemo_SwiftUI/CordialDemo_SwiftUI/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         CatalogView()
+            .onOpenURL(perform: { url in
+                print(url)
+            })
     }
 }
 
