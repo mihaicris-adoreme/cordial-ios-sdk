5e7d05433d6cfb15adda247b57c14608d1eccd56
Yan Malinovsky <yan.malinovsky@gmail.com>
2021-11-12 14:08:19 +0200

    Fixed bug with vanity deepLinks in the case when convert has been failed

---
+++ Sources/CordialSDK/DeepLinks/DeepLink/SwiftUI/CordialSwiftUIDeepLinksHandler.swift
@@ -0,0 +1,53 @@
+//
+//  CordialSwiftUIDeepLinksHandler.swift
+//  CordialSDK
+//
+//  Created by Yan Malinovsky on 18.06.2021.
+//  Copyright © 2021 cordial.io. All rights reserved.
+//
+
+import Foundation
+import os.log
+
+public class CordialSwiftUIDeepLinksHandler {
+    
+    public init() {}
+    
+    @available(iOS 13.0, *)
+    public func processDeepLink(url: URL) {
+        NotificationManager.shared.originDeepLink = url.absoluteString
+        
+        InternalCordialAPI().sentEventDeepLinkOpen()
+        
+        CordialVanityDeepLink().getDeepLink(url: url, onSuccess: { url in
+            CordialSwiftUIDeepLinksPublisher.shared.publishDeepLink(url: url, fallbackURL: nil, completionHandler: { deepLinkActionType in
+
+                InternalCordialAPI().deepLinkAction(deepLinkActionType: deepLinkActionType)
+            })
+            
+            NotificationManager.shared.vanityDeepLink = String()
+            
+            if CordialApiConfiguration.shared.osLogManager.isAvailableOsLogLevelForPrint(osLogLevel: .info) {
+                os_log("Vanity DeepLink converted successfully", log: OSLog.cordialDeepLinks, type: .info)
+            }
+        }, onFailure: { error in
+            if !NotificationManager.shared.vanityDeepLink.isEmpty, let vanityDeepLinkURL = URL(string: NotificationManager.shared.vanityDeepLink) {
+                CordialSwiftUIDeepLinksPublisher.shared.publishDeepLink(url: vanityDeepLinkURL, fallbackURL: nil, completionHandler: { deepLinkActionType in
+                    
+                    InternalCordialAPI().deepLinkAction(deepLinkActionType: deepLinkActionType)
+                })
+            } else {
+                CordialSwiftUIDeepLinksPublisher.shared.publishDeepLink(url: url, fallbackURL: nil, completionHandler: { deepLinkActionType in
+                    
+                    InternalCordialAPI().deepLinkAction(deepLinkActionType: deepLinkActionType)
+                })
+            }
+            
+            NotificationManager.shared.vanityDeepLink = String()
+            
+            if CordialApiConfiguration.shared.osLogManager.isAvailableOsLogLevelForPrint(osLogLevel: .error) {
+                os_log("Vanity DeepLink opening failed. Error: [%{public}@]", log: OSLog.cordialDeepLinks, type: .error, error)
+            }
+        })
+    }
+}
