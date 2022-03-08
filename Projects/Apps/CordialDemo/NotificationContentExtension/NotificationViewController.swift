d043dad7cf3ae5ff61f8da4c21d020123d91e0fb
Yan Malinovsky <yan.malinovsky@gmail.com>
2022-03-08 16:06:28 +0200

    Added programmatically CarouselView base setup to NotificationContentExtension

--- Projects/Apps/CordialDemo/NotificationContentExtension/NotificationViewController.swift
+++ Projects/Apps/CordialDemo/NotificationContentExtension/NotificationViewController.swift
@@ -10,25 +10,67 @@ import UIKit
 import UserNotifications
 import UserNotificationsUI
 
-class NotificationViewController: UIViewController, UNNotificationContentExtension {
+class NotificationViewController: UIViewController, UNNotificationContentExtension, CarouselViewDelegate {
 
-    @IBOutlet var label: UILabel?
+    // MARK: - Subvies
+    
+    private var carouselView: CarouselView?
+    
+    // MARK: - Properties
+    
+    private var carouselData = [CarouselView.CarouselData]()
+    private let backgroundColors: [UIColor] = [#colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1), #colorLiteral(red: 0.4826081395, green: 0.04436998069, blue: 0.2024421096, alpha: 1), #colorLiteral(red: 0.1728022993, green: 0.42700845, blue: 0.3964217603, alpha: 1)]
+    
+    // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any required interface initialization here.
         
+        self.carouselView = CarouselView(pages: 3, delegate: self)
+        self.carouselData.append(.init(image: UIImage(named: "Image_1"), text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"))
+        self.carouselData.append(.init(image: UIImage(named: "Image_2"), text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"))
+        self.carouselData.append(.init(image: UIImage(named: "Image_3"), text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"))
+        
+        self.setupUI()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.carouselView?.configureView(with: carouselData)
     }
     
     override func loadView() {
         super.loadView()
         
         let size = view.bounds.size
-        preferredContentSize = CGSize(width: size.width, height: size.width)
+        self.preferredContentSize = CGSize(width: size.width, height: size.width)
     }
     
     func didReceive(_ notification: UNNotification) {
-        self.label?.text = notification.request.content.body
+//        self.label?.text = notification.request.content.body
     }
 
+    // MARK: - Setups
+
+    private func setupUI() {
+        self.view.backgroundColor = self.backgroundColors.first
+        
+        guard let carouselView = self.carouselView else { return }
+        self.view.addSubview(carouselView)
+        carouselView.translatesAutoresizingMaskIntoConstraints = false
+        carouselView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        carouselView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        carouselView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        carouselView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+
+    // MARK: - CarouselViewDelegate
+
+    func currentPageDidChange(to page: Int) {
+        UIView.animate(withDuration: 0.7) {
+            self.view.backgroundColor = self.backgroundColors[page]
+        }
+    }
 }
