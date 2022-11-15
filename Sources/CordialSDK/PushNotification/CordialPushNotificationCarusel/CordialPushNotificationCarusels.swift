be7860deaad0f9c9b4de6075a002777d3a923629
Yan Malinovsky <yan.malinovsky@gmail.com>
2022-11-15 14:22:26 +0200

    Added CordialPushNotificationsCarusels base class

---
+++ Sources/CordialSDK/PushNotification/CordialPushNotificationCarusel/CordialPushNotificationCarusels.swift
@@ -0,0 +1,16 @@
+//
+//  CordialPushNotificationCarusels.swift
+//  CordialSDK
+//
+//  Created by Yan Malinovsky on 15.11.2022.
+//  Copyright © 2022 cordial.io. All rights reserved.
+//
+
+import Foundation
+
+struct CordialPushNotificationCarusels {
+    
+    let image: UIImage
+    let deepLink: URL
+    
+}
---
+++ Sources/CordialSDK/PushNotification/CordialPushNotificationCarusel/CordialPushNotificationsCarusels.swift
@@ -0,0 +1,15 @@
+//
+//  CordialPushNotificationsCarusels.swift
+//  CordialSDK
+//
+//  Created by Yan Malinovsky on 15.11.2022.
+//  Copyright © 2022 cordial.io. All rights reserved.
+//
+
+import Foundation
+
+struct CordialPushNotificationsCarusels {
+    
+    let value: Dictionary<String, CordialPushNotificationCarusels>
+    
+}
