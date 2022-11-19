f3f458d2b31bdeb2417eaf25e4bbf70499769d42
Yan Malinovsky <yan.malinovsky@gmail.com>
2022-11-19 21:18:04 +0200

    Renamed CarouselNotificationExtension to NotificationManager at CordialAppExtension

---
+++ Sources/CordialAppExtensions/API/NotificationManager.swift
@@ -0,0 +1,15 @@
+//
+//  NotificationManager.swift
+//  NotificationContentExtension
+//
+//  Created by Yan Malinovsky on 21.03.2022.
+//  Copyright © 2022 cordial.io. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    
+    static let didReceiveCarouselsNotification = Notification.Name("CordialDidReceiveCarouselsNotification")
+    
+}
