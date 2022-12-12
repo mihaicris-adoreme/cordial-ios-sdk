66a941398c9ef6bb21f55b3308831b78257575be
Yan Malinovsky <yan.malinovsky@gmail.com>
2022-12-12 15:47:45 +0200

    Added base AppIntentsExtension classes to demo app

---
+++ Projects/Apps/CordialDemo/CordialDemoAppIntents/CordialDemoAppIntentsExtension.swift
@@ -0,0 +1,13 @@
+//
+//  CordialDemoAppIntentsExtension.swift
+//  CordialDemoAppIntents
+//
+//  Created by Yan Malinovsky on 12.12.2022.
+//  Copyright © 2022 cordial.io. All rights reserved.
+//
+
+import AppIntents
+
+@main
+struct CordialDemoAppIntentsExtension: AppIntentsExtension {
+}
