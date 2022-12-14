26fd2013c923a9285ca399de90773a08aecbf79e
Yan Malinovsky <yan.malinovsky@gmail.com>
2022-12-14 16:48:47 +0200

    CordialSDK ver 4.1.2

--- Sources/CordialSDK/API/CordialApiConfiguration.swift
+++ Sources/CordialSDK/API/CordialApiConfiguration.swift
@@ -16,7 +16,7 @@ import os.log
     
     private override init() {}
     
-    let sdkVersion = "4.1.1"
+    let sdkVersion = "4.1.2"
     
     let initReachabilityManagerSingleton = ReachabilityManager.shared
     let initReachabilitySenderSingleton = ReachabilitySender.shared
