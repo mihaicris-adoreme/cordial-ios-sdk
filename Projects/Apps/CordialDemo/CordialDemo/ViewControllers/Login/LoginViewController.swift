e9ac5aff66d6588184bb300089a105ee3f71336a
Yan Malinovsky <yan.malinovsky@gmail.com>
2023-01-13 16:25:40 +0200

    Added sound to provisional push notifications at the demo app guest request

--- Projects/Apps/CordialDemo/CordialDemo/ViewControllers/Login/LoginViewController.swift
+++ Projects/Apps/CordialDemo/CordialDemo/ViewControllers/Login/LoginViewController.swift
@@ -63,7 +63,7 @@ class LoginViewController: UIViewController {
     @IBAction func guestAction(_ sender: UIButton) {
         self.deleteAppCoreData()
         
-        self.cordialAPI.registerForPushNotifications(options: [.provisional])
+        self.cordialAPI.registerForPushNotifications(options: [.provisional, .sound])
         
         self.cordialAPI.setContact(primaryKey: nil)
         
