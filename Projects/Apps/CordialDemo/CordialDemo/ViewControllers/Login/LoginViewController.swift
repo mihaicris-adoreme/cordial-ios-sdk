1f7eaa6bee1d4d32b0240bc02b8ff95417f5113d
Yan Malinovsky <yan.malinovsky@gmail.com>
2023-02-17 16:20:55 +0200

    Reverted moving app notification settings param to SDK push notifications register

--- Projects/Apps/CordialDemo/CordialDemo/ViewControllers/Login/LoginViewController.swift
+++ Projects/Apps/CordialDemo/CordialDemo/ViewControllers/Login/LoginViewController.swift
@@ -47,7 +47,7 @@ class LoginViewController: UIViewController {
             if !primaryKey.isEmpty {
                 self.cordialAPI.setContact(primaryKey: primaryKey)
                 
-                self.cordialAPI.registerForPushNotifications(options: [.alert, .sound])
+                self.cordialAPI.registerForPushNotifications(options: [.alert, .sound, .providesAppNotificationSettings])
                 
                 App.userLogIn()
                 
@@ -63,7 +63,7 @@ class LoginViewController: UIViewController {
     @IBAction func guestAction(_ sender: UIButton) {
         self.deleteAppCoreData()
         
-        self.cordialAPI.registerForPushNotifications(options: [.alert, .sound, .provisional])
+        self.cordialAPI.registerForPushNotifications(options: [.alert, .sound, .provisional, .providesAppNotificationSettings])
         
         self.cordialAPI.setContact(primaryKey: nil)
         
--- Sources/CordialSDK/CordialAPI.swift
+++ Sources/CordialSDK/CordialAPI.swift
@@ -180,13 +180,6 @@ import os.log
     
     @objc public func registerForPushNotifications(options: UNAuthorizationOptions) {
         if CordialApiConfiguration.shared.pushesConfiguration == .SDK {
-            
-            if !InternalCordialAPI().getPushNotificationSettings().isEmpty,
-               !options.contains(.providesAppNotificationSettings) {
-                
-                CordialPushNotification.shared.registerForPushNotifications(options: .providesAppNotificationSettings)
-            }
-            
             CordialPushNotification.shared.registerForPushNotifications(options: options)
         } else {
             if CordialApiConfiguration.shared.osLogManager.isAvailableOsLogLevelForPrint(osLogLevel: .info) {
