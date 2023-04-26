31c88c1aba686ccb579dcf41c9c433dfb829c574
Yan Malinovsky <yan.malinovsky@gmail.com>
2023-04-26 19:41:05 +0300

    Added func to saving demo logs to file by rows

--- Projects/Apps/CordialDemo/CordialDemo/OSLog/OSLogManager.swift
+++ Projects/Apps/CordialDemo/CordialDemo/OSLog/OSLogManager.swift
@@ -14,5 +14,33 @@ extension OSLog {
     private static var subsystem = Bundle.main.bundleIdentifier!
     
     static let сordialSDKDemo = OSLog(subsystem: subsystem, category: "CordialSDKDemo")
+        
+    func loging(row: String) {
+        let folderName = "logs"
+        let fileName = "\(OSLog.subsystem).log"
+        
+        let folderURL = self.getDocumentsDirectory().appendingPathComponent(folderName, isDirectory: true)
+        
+        do {
+            try FileManager.default.createDirectory(at: folderURL, withIntermediateDirectories: true, attributes: nil)
+            
+            let fileURL = folderURL.appendingPathComponent(fileName)
+            
+            let fileUpdater = try FileHandle(forUpdating: fileURL)
+            
+            fileUpdater.seekToEndOfFile()
+            fileUpdater.write(row.data(using: .utf8)!)
+            fileUpdater.closeFile()
+            
+        } catch let error {
+            os_log("Error: [%{public}@]", log: .сordialSDKDemo, type: .error, error.localizedDescription)
+        }
+    }
     
+    private func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        
+        return documentsDirectory
+    }
 }
