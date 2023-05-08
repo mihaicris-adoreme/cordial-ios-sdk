dbaadab56defacecb785af17b11c993e53221ec9
Yan Malinovsky <yan.malinovsky@gmail.com>
2023-05-08 16:30:54 +0300

    Added empty LogsViewController

---
+++ Projects/Apps/CordialDemo/CordialDemo/ViewControllers/Logs/LogsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LogsViewController.swift
+//  CordialDemo
+//
+//  Created by Yan Malinovsky on 08.05.2023.
+//  Copyright © 2023 cordial.io. All rights reserved.
+//
+
+import UIKit
+
+class LogsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Logs"
+    }
+    
+}
