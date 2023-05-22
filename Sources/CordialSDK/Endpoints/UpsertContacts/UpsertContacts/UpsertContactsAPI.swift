7d606d998c1eab954da1191091004fbfe2a77729
Yan Malinovsky <yan.malinovsky@gmail.com>
2023-05-22 15:19:48 +0300

    Refactoring UpsertContacts folder structure

---
+++ Sources/CordialSDK/Endpoints/UpsertContacts/UpsertContacts/UpsertContactsAPI.swift
@@ -0,0 +1,15 @@
+//
+//  UpsertContactsAPI.swift
+//  CordialSDK
+//
+//  Created by Yan Malinovsky on 22.05.2023.
+//  Copyright © 2023 cordial.io. All rights reserved.
+//
+
+import Foundation
+
+@objcMembers public class UpsertContactsAPI: NSObject {
+    
+    
+    
+}
