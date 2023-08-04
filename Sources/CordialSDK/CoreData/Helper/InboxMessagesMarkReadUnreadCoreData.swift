46467ec16b73bfd4547f3b87d465faf86c075c56
Yan Malinovsky <yan.malinovsky@gmail.com>
2023-08-04 14:57:27 +0300

    Changed the way for setting data at InboxMessagesMarkReadUnreadCoreData

--- Sources/CordialSDK/CoreData/Helper/InboxMessagesMarkReadUnreadCoreData.swift
+++ Sources/CordialSDK/CoreData/Helper/InboxMessagesMarkReadUnreadCoreData.swift
@@ -13,26 +13,65 @@ class InboxMessagesMarkReadUnreadCoreData {
     
     let entityName = "InboxMessagesReadUnreadMarks"
     
+    // MARK: Setting Data
+    
     func putInboxMessagesMarkReadUnreadRequest(inboxMessagesMarkReadUnreadRequest: InboxMessagesMarkReadUnreadRequest) {
         guard let context = CoreDataManager.shared.persistentContainer?.viewContext else { return }
         
         if let entity = NSEntityDescription.entity(forEntityName: self.entityName, in: context) {
-            let newRow = NSManagedObject(entity: entity, insertInto: context)
+            let requestID = inboxMessagesMarkReadUnreadRequest.requestID
             
-            do {
-                let inboxMessagesMarkReadUnreadRequestArchivedData = try NSKeyedArchiver.archivedData(withRootObject: inboxMessagesMarkReadUnreadRequest, requiringSecureCoding: true)
-        
-                newRow.setValue(inboxMessagesMarkReadUnreadRequestArchivedData, forKey: "data")
-                newRow.setValue(inboxMessagesMarkReadUnreadRequest.date, forKey: "date")
-                
-                try context.save()
-            } catch let error {
-                CoreDataManager.shared.deleteAll(entityName: self.entityName)
-                
-                LoggerManager.shared.error(message: "CoreData Error: [\(error.localizedDescription)] Entity: [\(self.entityName)]", category: "CordialSDKCoreDataError")
+            guard let isInboxMessagesMarkReadUnreadRequest = CoreDataManager.shared.isRequestObjectExist(requestID: requestID, entityName: self.entityName) else { return }
+            
+            if isInboxMessagesMarkReadUnreadRequest {
+                self.updateInboxMessagesMarkReadUnreadRequest(inboxMessagesMarkReadUnreadRequest: inboxMessagesMarkReadUnreadRequest)
+            } else {
+                let managedObject = NSManagedObject(entity: entity, insertInto: context)
+                self.setInboxMessagesMarkReadUnreadRequest(managedObject: managedObject, context: context, inboxMessagesMarkReadUnreadRequest: inboxMessagesMarkReadUnreadRequest)
             }
         }
     }
+    
+    private func setInboxMessagesMarkReadUnreadRequest(managedObject: NSManagedObject, context: NSManagedObjectContext, inboxMessagesMarkReadUnreadRequest: InboxMessagesMarkReadUnreadRequest) {
+        do {
+            let inboxMessagesMarkReadUnreadRequestArchivedData = try NSKeyedArchiver.archivedData(withRootObject: inboxMessagesMarkReadUnreadRequest, requiringSecureCoding: true)
+    
+            managedObject.setValue(inboxMessagesMarkReadUnreadRequestArchivedData, forKey: "data")
+            managedObject.setValue(inboxMessagesMarkReadUnreadRequest.date, forKey: "date")
+            managedObject.setValue(inboxMessagesMarkReadUnreadRequest.requestID, forKey: "requestID")
+            managedObject.setValue(false, forKey: "flushing")
+            
+            CoreDataManager.shared.saveContext(context: context, entityName: self.entityName)
+            
+        } catch let error {
+            CoreDataManager.shared.deleteAll(entityName: self.entityName)
+            
+            LoggerManager.shared.error(message: "CoreData Error: [\(error.localizedDescription)] Entity: [\(self.entityName)]", category: "CordialSDKCoreDataError")
+        }
+    }
+    
+    private func updateInboxMessagesMarkReadUnreadRequest(inboxMessagesMarkReadUnreadRequest: InboxMessagesMarkReadUnreadRequest) {
+        guard let context = CoreDataManager.shared.persistentContainer?.viewContext else { return }
+
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: self.entityName)
+        request.returnsObjectsAsFaults = false
+        
+        request.predicate = NSPredicate(format: "requestID = %@", inboxMessagesMarkReadUnreadRequest.requestID)
+        
+        do {
+            guard let managedObjects = try context.fetch(request) as? [NSManagedObject] else { return }
+            
+            for managedObject in managedObjects {
+                self.setInboxMessagesMarkReadUnreadRequest(managedObject: managedObject, context: context, inboxMessagesMarkReadUnreadRequest: inboxMessagesMarkReadUnreadRequest)
+            }
+        } catch let error {
+            CoreDataManager.shared.deleteAll(entityName: self.entityName)
+            
+            LoggerManager.shared.error(message: "CoreData Error: [\(error.localizedDescription)] Entity: [\(self.entityName)]", category: "CordialSDKCoreDataError")
+        }
+    }
+    
+    // MARK: Getting Data
     
     func fetchInboxMessagesMarkReadUnreadRequests() -> [InboxMessagesMarkReadUnreadRequest] {
         var inboxMessagesMarkReadUnreadRequests = [InboxMessagesMarkReadUnreadRequest]()
